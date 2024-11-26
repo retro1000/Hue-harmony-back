7f3ae5847a7dd17c2ac90d1387f80b640049b430
retro1000 <damithajayawardana678@gmail.com>
2024-11-26 19:50:44 +0530

    update

--- web/src/main/java/hueHarmony/web/service/ProductService.java
+++ web/src/main/java/hueHarmony/web/service/ProductService.java
@@ -17,6 +17,7 @@ import hueHarmony.web.util.ConvertUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
