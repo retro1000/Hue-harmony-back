0c35d1312a02f5c5e30727af25bc0e32065e2b02
retro1000 <damithajayawardana678@gmail.com>
2024-11-27 15:29:24 +0530

    update

--- web/src/main/java/hueHarmony/web/controller/Order.java
+++ web/src/main/java/hueHarmony/web/controller/Order.java
@@ -10,63 +10,63 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/orders")
 public class Order {
 
-//    @PostMapping("/place-order/online")
-//    @PreAuthorize("hasRole('ROLE_USER')")
-//    public ResponseEntity<Object> placeNewOrderOnline(){
-//        try{
-//
-//        }catch(Exception e){
-//            return ResponseEntity.status(500).body("Internal Server Error");
-//        }
-//    }
-//
-//    @PostMapping("/place-order/shop")
-//    @PreAuthorize("hasRole('ROLE_CACHIER')")
-//    public ResponseEntity<Object> placeNewOrderShop(){
-//        try{
-//
-//        }catch(Exception e){
-//            return ResponseEntity.status(500).body("Internal Server Error");
-//        }
-//    }
-//
-//    @PostMapping("/create-order")
-//    @PreAuthorize("hasRole('ROLE_BACKOFFICE')")
-//    public ResponseEntity<Object> createOrder(){
-//        try{
-//
-//        }catch(Exception e){
-//            return ResponseEntity.status(500).body("Internal Server Error");
-//        }
-//    }
-//
-//    @GetMapping("/view/{orderId}")
-//    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_SALESMANAGER', 'ROLE_BACKOFFICE')")
-//    public ResponseEntity<Object> viewOrder(@PathVariable("orderId") int orderId){
-//        try{
-//
-//        }catch(Exception e){
-//            return ResponseEntity.status(500).body("Internal Server Error");
-//        }
-//    }
-//
-//    @GetMapping("/filter")
-//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SALESMANAGER', 'ROLE_BACKOFFICE')")
-//    public ResponseEntity<Object> filterOrders(@Validated(FilterOrderDto.whenOrganization.class) @ModelAttribute FilterOrderDto request) {
-//        try{
-//
-//        }catch(Exception e){
-//            return ResponseEntity.status(500).body("Internal Server Error");
-//        }
-//    }
-//
-//    @GetMapping("/filter-my-orders")
-//    @PreAuthorize("hasRole('ROLE_USER')")
-//    public ResponseEntity<Object> filterMyOrders(@Validated(FilterOrderDto.whenUser.class) @ModelAttribute FilterOrderDto request) {
-//        try{
-//
-//        }catch(Exception e){
-//            return ResponseEntity.status(500).body("Internal Server Error");
-//        }
-//    }
+    @PostMapping("/place-order/online")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<Object> placeNewOrderOnline(){
+        try{
+            return ResponseEntity.status(200).body("");
+        }catch(Exception e){
+            return ResponseEntity.status(500).body("Internal Server Error");
+        }
+    }
+
+    @PostMapping("/place-order/shop")
+    @PreAuthorize("hasRole('ROLE_CACHIER')")
+    public ResponseEntity<Object> placeNewOrderShop(){
+        try{
+            return ResponseEntity.status(200).body("");
+        }catch(Exception e){
+            return ResponseEntity.status(500).body("Internal Server Error");
+        }
+    }
+
+    @PostMapping("/create-order")
+    @PreAuthorize("hasRole('ROLE_BACKOFFICE')")
+    public ResponseEntity<Object> createOrder(){
+        try{
+            return ResponseEntity.status(200).body("");
+        }catch(Exception e){
+            return ResponseEntity.status(500).body("Internal Server Error");
+        }
+    }
+
+    @GetMapping("/view/{orderId}")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_SALESMANAGER', 'ROLE_BACKOFFICE')")
+    public ResponseEntity<Object> viewOrder(@PathVariable("orderId") int orderId){
+        try{
+            return ResponseEntity.status(200).body("");
+        }catch(Exception e){
+            return ResponseEntity.status(500).body("Internal Server Error");
+        }
+    }
+
+    @GetMapping("/filter")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SALESMANAGER', 'ROLE_BACKOFFICE')")
+    public ResponseEntity<Object> filterOrders(@Validated(FilterOrderDto.whenOrganization.class) @ModelAttribute FilterOrderDto request) {
+        try{
+            return ResponseEntity.status(200).body("");
+        }catch(Exception e){
+            return ResponseEntity.status(500).body("Internal Server Error");
+        }
+    }
+
+    @GetMapping("/filter-my-orders")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<Object> filterMyOrders(@Validated(FilterOrderDto.whenUser.class) @ModelAttribute FilterOrderDto request) {
+        try{
+            return ResponseEntity.status(200).body("");
+        }catch(Exception e){
+            return ResponseEntity.status(500).body("Internal Server Error");
+        }
+    }
 }
--- web/src/main/java/hueHarmony/web/model/enums/data_set/ProductStatus.java
+++ web/src/main/java/hueHarmony/web/model/enums/data_set/ProductStatus.java
@@ -3,4 +3,5 @@ package hueHarmony.web.model.enums.data_set;
 public enum ProductStatus {
     UNAVAILABLE,
     AVAILABLE,
+    APPROVED
 }
