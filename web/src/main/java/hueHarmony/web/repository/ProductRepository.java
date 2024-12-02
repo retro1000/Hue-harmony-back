a66e1168a9b7e91146d6e67ecaf5633f33ed0477
Luwin Wickramasinghe <luwin.anuka@gmail.com>
2024-12-02 15:34:41 +0530

    custom query for get popular products

--- web/src/main/java/hueHarmony/web/repository/ProductRepository.java
+++ web/src/main/java/hueHarmony/web/repository/ProductRepository.java
@@ -1,6 +1,10 @@
 package hueHarmony.web.repository;
 
+import hueHarmony.web.dto.response.PopularProductsDto;
 import hueHarmony.web.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +13,17 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends ExtendedRepository<Product, Long>{
     List<Product> findByProductName(@Param("productName") String productName);
+
+    @Query(value = "SELECT p.product_id AS productId, " +
+            "p.product_name AS productName, " +
+            "STRING_AGG(pi.image_ids, ',') AS imageIds, " +
+            "p.product_price AS productPrice, " +
+            "p.product_discount AS productDiscount " +
+            "FROM Product p " +
+            "LEFT JOIN product_image_ids pi ON p.product_id = pi.product_product_id " +
+            "GROUP BY p.product_id, p.product_name, p.product_price, p.product_discount",
+            nativeQuery = true)
+    List<Object[]> findPopularProductsRaw(Pageable pageable);
+
 
 }
