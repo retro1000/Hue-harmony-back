a1a438498ec098619b8dca537da7215da8d21fba
Akila Kushantha <akila.kushantha@gmail.com>
2024-12-04 12:42:41 +0530

    grn create complete

--- web/src/main/java/hueHarmony/web/dto/UserAuthDto.java
+++ web/src/main/java/hueHarmony/web/dto/UserAuthDto.java
@@ -62,4 +62,30 @@ public class UserAuthDto implements UserDetails {
                 ", authorities=" + authorities +
                 '}';
     }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // Return true if account is not expired
+        return true; // Modify as per your logic
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // Return true if account is not locked
+        return true; // Modify as per your logic
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // Return true if credentials are not expired
+        return true; // Modify as per your logic
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // Return true if user is enabled
+        return true; // Modify as per your logic
+    }
+
 }
